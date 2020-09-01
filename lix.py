79cf0b5c0923de499d9048a7311ec48227399eee
Rasmus Edvardsen <edvardsen.rasmus@gmail.com>
2020-09-01 20:50:33 +0200

    Initial commit

---
+++ lix.py
@@ -0,0 +1,22 @@
+import sys
+import re
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 2 is str):
+        sys.exit(0)
+    file_name = sys.argv[1]
+
+    reader = open(file_name)
+    text = reader.read()    
+    reader.close()
+
+    text = text.replace("\n", " ").replace(":", ".")
+    B = text.count(".")
+    print("B: Number of periods", B)
+    A_list = text.replace('.', ' ').split()
+    print("A: Number of words", len(A_list))
+    A = len(A_list)
+    C = len([word for word in A_list if len(word) > 6])
+    print("C: Long words", C)
+    lix_ = (A/B) + ((C * 100)/A)
+    print(lix_)
